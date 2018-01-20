7d59a23c3a88029689c45b286b135a4d066315c7
Manuel Josafat Gomez Ituarte <josafat@zipdev.co>
2018-01-19 16:01:21 -0800

    Create account screen part 2

--- Smack/Smack/Controller/CreateAccountVC.swift
+++ Smack/Smack/Controller/CreateAccountVC.swift
@@ -15,14 +15,24 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     // Variables
-    var  avatarName = "ProfileDefault"
+    var avatarName = "ProfileDefault"
     var avatarColor = "[0.5,0.5,0.5,1]"
+    var backgroundColor: UIColor?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        setupView()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instance.avatarName != ""{
+            userImage.image = UIImage(named: UserDataService.instance.avatarName)
+            avatarName = UserDataService.instance.avatarName
+            if avatarName.contains("light") && backgroundColor == nil {
+                userImage.backgroundColor = UIColor.lightGray
+            }
+        }
     }
 
 
@@ -31,6 +41,8 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func createAccountPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
         guard let name = userNameText.text , userNameText.text != "" else { return }
         guard let email = emailText.text , emailText.text != "" else { return }
         guard let pass = passwordText.text , passwordText.text != "" else { return }
@@ -45,8 +57,11 @@ class CreateAccountVC: UIViewController {
                         AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
                             print("start")
                             if success{
+                                self.spinner.isHidden = true
+                                self.spinner.stopAnimating()
                                 print("made it", UserDataService.instance.name, UserDataService.instance.avatarName)
                                 self.performSegue(withIdentifier: UNWIND, sender: nil)
+                                NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
                             } else{
                                 print("Not successful!")
                             }
@@ -64,6 +79,27 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func pickBackgroundColorPressed(_ sender: Any) {
+        let r = CGFloat(arc4random_uniform(255)) / 255
+        let g = CGFloat(arc4random_uniform(255)) / 255
+        let b = CGFloat(arc4random_uniform(255)) / 255
+        
+        backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        UIView.animate(withDuration: 0.2){
+            self.userImage.backgroundColor = self.backgroundColor
+        }
     }
     
+    func setupView(){
+        spinner.isHidden = true
+        userNameText.attributedPlaceholder = NSAttributedString(string: "username", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+        emailText.attributedPlaceholder = NSAttributedString(string: "email", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+        passwordText.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(CreateAccountVC.handleTap))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleTap(){
+        view.endEditing(true)
+    }
 }
--- Smack/Smack/Utilities/Constants.swift
+++ Smack/Smack/Utilities/Constants.swift
@@ -16,6 +16,12 @@ let URL_REGISTER = "\(BASE_URL)/account/register"
 let LOGIN_USER = "\(BASE_URL)/account/login"
 let URL_USER_ADD = "\(BASE_URL)/user/add"
 
+//Colors
+let smackPurplePlaceholder = #colorLiteral(red: 0.3647058824, green: 0.4045833051, blue: 0.8775706887, alpha: 0.5)
+
+// Notifications
+let NOTIF_USER_DATA_DID_CHANGE = Notification.Name("notifUserDataDidChange")
+
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
